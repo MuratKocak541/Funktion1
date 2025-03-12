5277c78921121e8de8ccc65e2c1f3de12cae0344
MuratKocak541 <murat.kocal1@icloud.com>
2025-03-12 15:03:41 +0100

    Initial commit

---
+++ Aufgabe2.dart
@@ -0,0 +1,10 @@
+void triplePrint(String text) {
+ for (int i = 0; i < 3; i++) {
+  print(text);
+ }
+}
+void main() {
+ triplePrint("Hallo, Welt!");
+ triplePrint("Dart ist toll!");
+ triplePrint("Lernen macht Spaß!");
+}
